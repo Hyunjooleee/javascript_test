847ff24601472f7fdc0e4fc6a2a7c2b56f2f5c11
chaelin1211 <cofls749@naver.com>
2019-07-22 16:03:45 +0900

    commit

--- app.js
+++ app.js
@@ -9,18 +9,9 @@ const router = express.Router();
 
 
 router.get('/', function (req, res) {
-<<<<<<< HEAD
-res.sendFile(path.join(__dirname + '/main/output.html'));
-//__dirname : It will resolve to your project folder.
-});
-
-app.use('/', router);
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-=======
     res.sendFile(path.join(__dirname + '/main/output.html'));
     //__dirname : It will resolve to your project folder.
 });
 
 app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
->>>>>>> e1085f99bd2994f0343751d7274629afe7a7b880
