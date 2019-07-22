f9e64fec4688a828bbf6899b9e97d92d671916dd
chaelin1211 <cofls749@naver.com>
2019-07-22 15:34:26 +0900

    commit

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 app.use(express.static('public'))
 
 const path = require('path');
 const router = express.Router();
 router.get('/', function (req, res) {
-res.sendFile(path.join(__dirname + '/index.html'));
+res.sendFile(path.join(__dirname + '/main/output.html'));
 //__dirname : It will resolve to your project folder.
 });
 
 app.use('/', router);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
