6e391425a4fab5fb6f9566e11d91e7d0205dcd35
chaelin1211 <cofls749@naver.com>
2019-07-23 17:07:56 +0900

    commit

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ const router = express.Router();
 
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/main/output.html'));
+    res.sendFile(path.join(__dirname + '/main/event_onsubmit.html'));
     //__dirname : It will resolve to your project folder.
 });
 router.get('/switch', function (req, res) {
     res.sendFile(path.join(__dirname + '/main/switch.html'));
     //__dirname : It will resolve to your project folder.
 });
-
+router.get('/ans', function (req, res) {
+    console.log(req.query);
+    res.send('Request parameters : '+req.query.name+', '+req.query.password)
+});
 app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
