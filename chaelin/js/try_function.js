83767734c0a99f76fafcb3aefe2735ce3e74ca6b
chaelin1211 <cofls749@naver.com>
2019-07-24 13:37:44 +0900

    채ㅡㅡㅑㅅ

--- chaelin/js/try_function.js
+++ chaelin/js/try_function.js
@@ -30,3 +30,27 @@ function replace_string(text, t1, t2, id) {
     }
     document.getElementById(id).value = result;
 }
+/*  매개변수가 form일때
+
+    function replace_string(form) {
+    var ele = form.getElementsByTagName("input");
+    var str = ele[0];   str = str.value;
+    var t1=ele[1];  t1 = t1.value;
+    var t2=ele[2];  t2 = t2.value;
+    var output=ele[3];
+    var text = str.split(" "), i, result = "";
+    t1 = t1.trim();
+    t2 = t2.trim();
+    for (i = 0; i < text.length; i++) {
+        if (text[i] == t1) 
+            result += t2;
+        else if (text[i] == t2)
+            result += t1;
+        else
+            result += text[i];
+        result += " ";
+    }
+    output.value = result;
+    return false;
+}
+*/
